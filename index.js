8785c289812b50d146fb4df74ae3c7cfa690af21
Ahmed El Mustapha Jrad <ahmedelmustapha.jrad@insat.ucar.tn>
2026-02-02 21:38:30 +0100

    Sidebar and Header pleasde d'ont touch

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 document.querySelector(".loading").addEventListener("animationend",()=>{
     document.querySelector(".body").style.display= "flex";
 })
+
+document.querySelector('.nav-actions').addEventListener('click', (e) => {
+    if (e.offsetX > e.target.offsetWidth - 40) {
+        openNav();
+    }
+});
+
+function openNav() {
+    document.getElementById("side-menu").style.width = "300px";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+}
